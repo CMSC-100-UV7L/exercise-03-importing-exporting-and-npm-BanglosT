577dacc6a2e4807f360f006e79534165f6343dff
Thomas <jsbanglos@up.edu.ph>
2023-03-17 03:14:52 +0800

    docs: added documentation

--- functions.js
+++ functions.js
@@ -1,22 +1,32 @@
+// Banglos, John Thomas
+// CMSC 100 UV-7L
+
 const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 var validator = require('validator');
 
-
+// This functions concatenates first letter of first name, last name, and generated uuid
 function generateUniqueID(fname, lname){
-    
+
+    // uid is taken from uuid package; given standard uuid is 16 chars. long, substring is used to only get first 8 characters
     uid = uuidv4().substring(0,8)
+
+    //these are then concatenated together to form 
     accId = String(fname.toLowerCase().charAt(0)) + String(lname.toLowerCase()) + String(uid)
     return accId
 }
 
+// This function adds an account depending on the content of the input array
 function addAccount (arr){
+    //flags are used to determine the validity of input; getting 6 flags equates to a valid input
     var flags = 0;
 
+    //checks length of array 
     if(arr.length < 4){
         return console.log('Not enough array items')
     }
 
+    //checks if array items are string and number
     for (let i = 0; i < arr.length; i++) {
         if (i <= 2){
             if (typeof arr[i] === 'string'){
@@ -30,6 +40,7 @@ function addAccount (arr){
 
     }
 
+    // checks if string items are non-empty strings; if it is empty, loop breaks
     for (let i = 0; i < arr.length; i++) {
         if (i <= 2){
             if (arr[i] !== ""){
@@ -41,20 +52,23 @@ function addAccount (arr){
         flags += 1;
     }
 
+    //checks if age is at least 18
     if (arr[3] >= 18){
         flags += 1
     }
 
+    //validates email through validator package
     if (validator.isEmail(arr[2])){
         flags += 1
     }
 
-    
+    //checks if flags are 6; else, return false
     if (flags == 6){
         uid = generateUniqueID(arr[0],arr[1]);
         arr.push(uid);
         var content = "";
 
+        // content is used as placeholder to concatenate all array data which is then appended to users.txt
         for (let i = 0; i < arr.length; i++) {
             if (i < arr.length-1){
                 content += arr[i]+','
@@ -78,6 +92,6 @@ function addAccount (arr){
 }
 
 
-
+//exported with commonjs format
 module.exports.generateUniqueID = generateUniqueID;
 module.exports.addAccount = addAccount;
